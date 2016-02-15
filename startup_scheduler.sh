73b908c20a66684b98a356e39470b4bd11ebe53b
Florian Loedermann <floedermann@gmail.com>
2016-02-15 10:09:37 +0100

    Updated start script and modified swarm config

--- startup_scheduler.sh
+++ startup_scheduler.sh
@@ -3,9 +3,9 @@
 # set -x
 
 set -e
-echo WAITING 15...
+#echo WAITING 15...
 
-sleep 15s
+#sleep 15s
 
 echo CONTAINER_NAME: ${CONTAINER_NAME:=${0##*/}}
 echo DB_SERVER_DBMS: ${DB_SERVER_DBMS:=mysql}
@@ -14,6 +14,12 @@ echo DB_SERVER_PORT: ${DB_SERVER_PORT:=$MYSQL_PORT_3306_TCP_PORT}
 echo DB_SERVER_USER: ${DB_SERVER_USER:=$MYSQL_ENV_MYSQL_USER}
 echo DB_SERVER_PASSWORD: ${DB_SERVER_PASSWORD:=$MYSQL_ENV_MYSQL_PASSWORD}
 echo DB_SERVER_DATABASE: ${DB_SERVER_DATABASE:=$MYSQL_ENV_MYSQL_DATABASE}
+
+while ! curl http://$DB_SERVER_HOST:$DB_SERVER_PORT/
+do
+  echo "$(date) - waiting for mysql..."
+  sleep 1
+done
 
 sed -i -e "s/{{DB_SERVER_DBMS}}/$DB_SERVER_DBMS/g" /root/install/scheduler_install.xml
 sed -i -e "s/{{DB_SERVER_HOST}}/$DB_SERVER_HOST/g" /root/install/scheduler_install.xml
